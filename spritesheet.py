ef032223b3fc78290641e53babf6ffd5be7e894e
wesmags <66344550+wesmags@users.noreply.github.com>
2021-09-26 19:34:12 +1000

    Fixed sprite sheet issues.

--- spritesheet.py
+++ spritesheet.py
@@ -1,19 +1,20 @@
 import pygame
+from entity import *
 
 class Spritesheet:
     def __init__(self, filename, width, height, rows, cols):
-        image = pygame.image.load(filename).convert()
+        image = pygame.image.load(filename)
         self.tile_table = []
         for tile_x in range (0, cols):
             line = []
             self.tile_table.append(line)
             for tile_y in range(0, rows):
                 rect = (tile_x * width, tile_y * height, width, height)
-                line.append(image.pygame.Surface.subsurface(rect))
+                line.append(image.subsurface(rect))
 
     def getTile(self, x, y):
         return self.tile_table[x][y]
-
+    
     def draw(self, screen):
         for x, row in enumerate(self.tile_table):
             for y, tile in enumerate(row):
